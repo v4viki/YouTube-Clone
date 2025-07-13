2e9e94b7bf1a65b2d101108db6f765c81637b6ee
Vikas Ahirwar <v4vikasahirwar@gmail.com>
2025-07-13 13:09:54 +0530

    Delete tailwind.config.js

--- tailwind.config.js
+++
@@ -1,9 +0,0 @@
-module.exports = {
-  content: ['./src/**/*.{js,jsx,ts,tsx}', './public/index.html'], // Ensure this matches your file structure
-  theme: {
-    extend: {},
-  },
-  plugins: [
-    require('@tailwindcss/line-clamp'),
-  ],
-};
